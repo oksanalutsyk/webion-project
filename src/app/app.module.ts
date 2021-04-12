267f44777cbada14646536f2c224eddd5bce874f
olutsyk <o.olutsyk@crosssoft.de>
2021-04-12 12:58:31 +0300

    added main components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { AboutPageComponent } from './components/about-page/about-page.component';
+import { ServicesPageComponent } from './components/services-page/services-page.component';
+import { ProjectPageComponent } from './components/project-page/project-page.component';
+import { NewsPageComponent } from './components/news-page/news-page.component';
+import { ContactsPageComponent } from './components/contacts-page/contacts-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomePageComponent,
+    AboutPageComponent,
+    ServicesPageComponent,
+    ProjectPageComponent,
+    NewsPageComponent,
+    ContactsPageComponent
   ],
   imports: [
     BrowserModule,
